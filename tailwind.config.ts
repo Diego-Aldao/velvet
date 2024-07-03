1e25694430c721992b7fc0f5022e52d593bab95a
Diego Aldao <diegofrontdev@gmail.com>
2024-07-02 22:04:17 -0300

    inicio del proyecto

--- tailwind.config.ts
+++ tailwind.config.ts
@@ -8,10 +8,22 @@ const config: Config = {
   ],
   theme: {
     extend: {
-      backgroundImage: {
-        "gradient-radial": "radial-gradient(var(--tw-gradient-stops))",
-        "gradient-conic":
-          "conic-gradient(from 180deg at 50% 50%, var(--tw-gradient-stops))",
+      fontFamily: {
+        "inter-tight": "var(--font-inter)",
+        "nunito-sans": "var(--font-nunito-sans)",
+      },
+      transitionDuration: {
+        DEFAULT: "200ms",
+      },
+      transitionTimingFunction: {
+        DEFAULT: "cubic-bezier(0.165, 0.84, 0.44, 1)",
+      },
+      colors: {
+        "main-white": "#fdfdfd",
+        "main-black": "#0e0e10",
+        primary: "#a5e1cd",
+        "secondary-black": "#1a1a1a",
+        accent: "#a1a28e",
       },
     },
   },
