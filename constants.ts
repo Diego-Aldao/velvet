c037fba13bd04af6b420b5c1e8b52fc48c0c6d85
Diego Aldao <diegofrontdev@gmail.com>
2024-07-10 18:51:54 -0300

    añadido un listado para filtrar por orden el listao de productos, y la url base para el fetch a productos

---
+++ constants.ts
@@ -0,0 +1,24 @@
+import { LocalFilters } from "./types/localTypes";
+
+export const LISTADO_ORDEN_FILTROS_PRODUCTOS: LocalFilters[] = [
+  {
+    id: 1,
+    nombre: "mas nuevo",
+    value: "freshness",
+    isSelected: true,
+  },
+  {
+    id: 3,
+    nombre: "precio ascendente",
+    value: "pricedasc",
+    isSelected: false,
+  },
+  {
+    id: 2,
+    nombre: "precio descendente",
+    value: "pricedesc",
+    isSelected: false,
+  },
+];
+
+export const BASE_URL_PRODUCTOS = `https://asos2.p.rapidapi.com/products/v2/list?store=ES&lang=es-ES&country=ES&currency=EUR&sizeSchema=ES&`;
