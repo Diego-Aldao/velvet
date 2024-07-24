4cb3c7e1ec5d513ebed6164e078cf141e83c05ee
Diego Aldao <diegofrontdev@gmail.com>
2024-07-24 18:32:12 -0300

    ahora el boton puede recibir customStyles

--- components/buttons/PageInicio/GeneroButton.tsx
+++ components/buttons/PageInicio/GeneroButton.tsx
@@ -5,17 +5,23 @@ interface Props {
   nombre: string;
   path: string;
   checked: boolean;
+  customStyles?: string;
 }
 
-export default function GeneroButton({ nombre, path, checked }: Props) {
+export default function GeneroButton({
+  nombre,
+  path,
+  checked,
+  customStyles,
+}: Props) {
   return (
     <Link
       href={path}
-      className={`py-2 px-4 rounded-md  uppercase font-bold font-nunito-sans tracking-wider hover:bg-primary hover:text-main-black transition-colors ${
+      className={`py-2 px-4 rounded-md uppercase font-bold font-nunito-sans tracking-wider hover:bg-primary hover:text-main-black transition-colors ${
         checked
           ? "bg-primary text-main-black"
           : "bg-secondary-black text-main-white"
-      }`}
+      } ${customStyles}`}
     >
       {nombre}
     </Link>
