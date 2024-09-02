754c3296e992298fcbef463437b87b10f22d7a8d
Diego Aldao <diegofrontdev@gmail.com>
2024-09-02 15:14:26 -0300

    añadido un height minimo a la pagina

--- app/productos/[id]/page.tsx
+++ app/productos/[id]/page.tsx
@@ -8,7 +8,7 @@ interface Props {
 export default function Page({ params }: Props) {
   const { id } = params;
   return (
-    <main className="px-4 md:px-10 lg:px-12 pt-24 md:pt-32 lg:pt-48 xl:pt-52 pb-20">
+    <main className="px-4 md:px-10 lg:px-12 pt-24 md:pt-32 lg:pt-48 xl:pt-52 pb-20 min-h-[200vh]">
       <MainContent idCategoria={id} />
     </main>
   );
