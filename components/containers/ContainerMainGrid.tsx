517aab7332c7ddde81d4bc16303635e91d364c87
Diego Aldao <diegofrontdev@gmail.com>
2024-09-02 15:25:30 -0300

    terminados los estilos y agregada la prop customStyles

--- components/containers/ContainerMainGrid.tsx
+++ components/containers/ContainerMainGrid.tsx
@@ -2,11 +2,14 @@ import React from "react";
 
 interface Props {
   children: React.ReactNode;
+  customStyles: string;
 }
 
-export default function ContainerMainGrid({ children }: Props) {
+export default function ContainerMainGrid({ children, customStyles }: Props) {
   return (
-    <section className="grid grid-cols-[repeat(auto-fill,minmax(250px,1fr))] gap-4 2xl:grid-cols-4">
+    <section
+      className={`grid h-fit w-full grid-cols-2 sm:grid-cols-[repeat(auto-fill,minmax(200px,1fr))] gap-4 gap-x-2 lg:grid-cols-3  xl:grid-cols-4 ${customStyles}`}
+    >
       {children}
     </section>
   );
