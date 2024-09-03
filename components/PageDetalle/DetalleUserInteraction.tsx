e6391676ce33a1a6d42d068a5ebb1af22efdd4ff
Diego Aldao <diegofrontdev@gmail.com>
2024-09-03 17:33:22 -0300

    extraida la logica y los botones comprar y añadir a carrito a sus propios componentes, reorganizado y terminados los estilos del propio componente

--- components/PageDetalle/DetalleUserInteraction.tsx
+++ components/PageDetalle/DetalleUserInteraction.tsx
@@ -1,10 +1,8 @@
 "use client";
-
-import { useProductContext } from "@/context/ProductsContext";
 import { Variant } from "@/types/fetchTypes";
-import { LocalProductInteraction } from "@/types/localTypes";
 import React, { useState } from "react";
-import { useRouter } from "next/navigation";
+import AddCarritoButton from "../buttons/AddCarritoButton";
+import ComprarButton from "../buttons/ComprarButton";
 
 interface Props {
   variantes?: Variant[];
@@ -13,6 +11,7 @@ interface Props {
   color: string;
   imagen: string;
   id: number;
+  marca: string;
 }
 
 export default function DetalleUserInteraction({
@@ -22,81 +21,79 @@ export default function DetalleUserInteraction({
   color,
   imagen,
   id,
+  marca,
 }: Props) {
   const [selectedSize, setSelectedSize] = useState<string | null>(null);
   const [noVariantError, setNoVariantError] = useState<boolean>(false);
-  const { updateCarrito } = useProductContext();
-  const router = useRouter();
 
   const handleVariante = (tamaño: string) => {
     setSelectedSize(tamaño);
     setNoVariantError(false);
   };
 
-  const checkVariantAndUpdate = () => {
-    if (selectedSize === null) {
-      setNoVariantError(true);
-    } else {
-      const articuloCarrito: LocalProductInteraction = {
-        nombre: nombre,
-        color: color,
-        imagen: imagen,
-        variantes: variantes,
-        tamaño: selectedSize,
-        precioFinal: precioFinal,
-        id: id,
-      };
-      updateCarrito(articuloCarrito);
-    }
-  };
-
-  const handleComprar = () => {
-    checkVariantAndUpdate();
-    router.push("/checkout");
-  };
-  const handleAñadirAlCarrito = () => {
-    checkVariantAndUpdate();
-  };
-
   return (
     <div className="flex flex-col gap-4">
+      <div className="w-full flex items-center gap-4">
+        <span className="first-letter:uppercase  inline-block line-clamp-1 ">
+          talle:
+        </span>
+        <span
+          className={`flex items-center gap-2 relative transition-all ${
+            noVariantError
+              ? "visible opacity-100 top-0"
+              : "invisible opacity-0 top-1"
+          }`}
+        >
+          <span className="icon-[tabler--alert-circle-filled] h-5 w-5 text-main-red"></span>
+          <span className="text-xs lg:text-sm first-letter:uppercase text-main-red font-semibold">
+            elegi una opción
+          </span>
+        </span>
+      </div>
       {variantes && (
-        <div className="flex flex-col gap-2 sm:flex-row sm:items-center md:flex-col md:items-start lg:flex-row lg:items-center">
-          <span className="first-letter:uppercase">elegir tamaño:</span>
-          <ul className="flex gap-2">
-            {variantes.map((variante) => (
-              <li
-                key={variante.id}
-                onClick={() => handleVariante(variante.brandSize)}
-                className={`rounded-full p-2 w-10 h-10 md:w-9 md:h-9 xl:w-10 xl:h-10 flex border border-transparent hover:bg-primary hover:text-main-black items-center justify-center transition-colors ${
-                  selectedSize === variante.brandSize
-                    ? "bg-primary !text-main-black"
-                    : "bg-secondary-black text-main-white"
-                } ${
-                  noVariantError
-                    ? "border-[#c20e4d] text-[#c20e4d]"
-                    : "border-transparent text-main-white"
-                }`}
-              >
-                <span className="text-sm font-bold">{variante.brandSize}</span>
-              </li>
-            ))}
-          </ul>
-        </div>
+        <ul className="flex gap-2 flex-wrap w-full items-center">
+          {variantes.map((variante) => (
+            <li
+              key={variante.id}
+              onClick={() => handleVariante(variante.brandSize)}
+              className={`cursor-pointer rounded-full p-1 px-3 flex border hover:bg-primary hover:border-primary hover:text-main-black items-center justify-center transition-colors ${
+                selectedSize === variante.brandSize
+                  ? "bg-primary !text-main-black"
+                  : "bg-secondary-black text-main-white"
+              } ${
+                noVariantError
+                  ? "border-[#c20e4d] text-[#c20e4d]"
+                  : "border-transparent text-main-white"
+              }`}
+            >
+              <span className="text-sm font-semibold font-nunito-sans">
+                {variante.brandSize}
+              </span>
+            </li>
+          ))}
+        </ul>
       )}
       <div className="w-full flex flex-col sm:flex-row md:flex-col gap-4">
-        <button
-          onClick={handleComprar}
-          className="py-2 px-3 uppercase font-bold text-sm border border-primary bg-primary text-main-black rounded-full w-full md:px-3"
-        >
-          comprar ahora
-        </button>
-        <button
-          onClick={handleAñadirAlCarrito}
-          className="py-2 px-3 uppercase text-primary font-semibold text-sm border border-primary rounded-full w-full "
-        >
-          añadir al carrito
-        </button>
+        <ComprarButton
+          selectedSize={selectedSize}
+          setNoVariantError={setNoVariantError}
+          nombre={nombre}
+          color={color}
+          imagen={imagen}
+          marca={marca}
+          precioFinal={precioFinal}
+          id={id}
+        />
+        <AddCarritoButton
+          selectedSize={selectedSize}
+          setNoVariantError={setNoVariantError}
+          nombre={nombre}
+          color={color}
+          imagen={imagen}
+          marca={marca}
+          precioFinal={precioFinal}
+          id={id}
+        />
       </div>
     </div>
   );
