127346fe5e2e51dbf82b5c2d0caed40821cc4537
Diego Aldao <diegofrontdev@gmail.com>
2024-09-18 18:53:45 -0300

    cambiados los links del componente Link a simplemente un li, para que el usuario no interactue con esa navegacion, y sea solo de guia

--- components/ChekoutFlow/LinksFlow.tsx
+++ components/ChekoutFlow/LinksFlow.tsx
@@ -1,7 +1,6 @@
 "use client";
 import React from "react";
 import { usePathname } from "next/navigation";
-import Link from "next/link";
 
 const listadoLinksFlow = [
   {
@@ -27,19 +26,20 @@ export default function LinksFlow() {
     <nav>
       <ul className="flex gap-2 items-center">
         {listadoLinksFlow.map((link) => (
-          <li className="text-xs capitalize" key={link.id}>
-            <Link href={link.destino} className="flex items-center gap-2">
-              {link.id >= 2 && (
-                <span className="icon-[tabler--chevron-right] text-main-white/50"></span>
-              )}
-              <span
-                className={`${
-                  !pathname.includes(link.destino) && "text-main-white/50"
-                }`}
-              >
-                {link.nombre}
-              </span>
-            </Link>
+          <li
+            className="text-xs capitalize flex items-center gap-2"
+            key={link.id}
+          >
+            {link.id >= 2 && (
+              <span className="icon-[tabler--chevron-right] text-main-white/50"></span>
+            )}
+            <span
+              className={`${
+                !pathname.includes(link.destino) && "text-main-white/50"
+              }`}
+            >
+              {link.nombre}
+            </span>
           </li>
         ))}
       </ul>
