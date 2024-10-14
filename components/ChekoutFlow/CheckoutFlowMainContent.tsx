3c2c1a12818c7c74be44d5d64f9a68b46476fddb
Diego Aldao <diegofrontdev@gmail.com>
2024-10-14 13:39:30 -0300

    añadido un effect para añadir a local storage el envio seleccionado, añadida la prop customStyles al componente Orden dentro de MainContentCheckout y añadido el children del componente MainContentConfirmacion

--- components/ChekoutFlow/CheckoutFlowMainContent.tsx
+++ components/ChekoutFlow/CheckoutFlowMainContent.tsx
@@ -1,11 +1,12 @@
 "use client";
-import React, { useState } from "react";
+import React, { useEffect, useState } from "react";
 import MainContentCarrito from "./Carrito/MainContentCarrito";
 import MainContentCheckout from "./Checkout/MainContentCheckout";
 import { Envio } from "@/types/localTypes";
 import Orden from "./Checkout/Orden";
 import MainContentPago from "./Pago/MainContentPago";
 import MainContentConfirmacion from "./Confirmacion/MainContentConfirmacion";
+import Link from "next/link";
 interface Props {
   checkoutPage: "carrito" | "checkout" | "pago" | "confirmacion";
 }
@@ -18,6 +19,11 @@ export default function CheckoutFlowMainContent({ checkoutPage }: Props) {
 
   const [currentEnvio, setCurrentEnvio] = useState<Envio | null>(null);
   const [direccionCorrecta, setDireccionCorrecta] = useState<boolean>(false);
+
+  useEffect(() => {
+    if (currentEnvio == null) return;
+    localStorage.setItem("currentEnvio", JSON.stringify(currentEnvio));
+  }, [currentEnvio]);
 
   return (
     <>
@@ -32,6 +38,7 @@ export default function CheckoutFlowMainContent({ checkoutPage }: Props) {
           <Orden
             currentEnvio={currentEnvio}
             direccionCorrecta={direccionCorrecta}
+            customStyles="bg-secondary-black px-3 py-4 border border-main-white/20 rounded-md md:row-start-1 md:col-start-2 md:mt-[14px] md:row-span-full md:sticky sm:top-0"
           >
             <button
               className={`w-full rounded-md py-2 ${
@@ -47,7 +54,25 @@ export default function CheckoutFlowMainContent({ checkoutPage }: Props) {
         </MainContentCheckout>
       )}
       {pagePago && <MainContentPago />}
-      {pageConfirmacion && <MainContentConfirmacion />}
+      {pageConfirmacion && (
+        <MainContentConfirmacion>
+          <Orden
+            currentEnvio={currentEnvio}
+            direccionCorrecta={direccionCorrecta}
+            pageConfirmacion={pageConfirmacion}
+            customStyles=""
+          >
+            <Link
+              href="/"
+              className="w-full rounded-md py-2 bg-primary flex items-center justify-center"
+            >
+              <span className="uppercase text-main-black font-bold">
+                seguir comprando
+              </span>
+            </Link>
+          </Orden>
+        </MainContentConfirmacion>
+      )}
     </>
   );
 }
