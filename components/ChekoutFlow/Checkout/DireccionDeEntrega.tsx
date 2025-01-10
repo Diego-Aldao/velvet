0736531cffc54c403391331afbb1f25cbc67695c
Diego Aldao <diegofrontdev@gmail.com>
2025-01-10 19:59:08 -0300

    añadido useForm de react-hook-form, añadidas las propiedades requeridas en los objetos del listadoInputsEntrega, añadidas las nuevas props a los componentes CustomFieldet, CustomInput y Select, y añadido un boton para hacer el submit del formulario

--- components/ChekoutFlow/Checkout/DireccionDeEntrega.tsx
+++ components/ChekoutFlow/Checkout/DireccionDeEntrega.tsx
@@ -1,8 +1,10 @@
+"use client";
 import React from "react";
 import CustomFieldset from "./CustomFieldset";
 import CustomInput from "@/components/inputs/CustomInput";
-import { LISTADO_DE_PAISES } from "@/data/data";
+import { LISTADO_PAISES_ENVIO } from "@/constants";
 import { Select, SelectItem } from "@nextui-org/select";
+import { useForm } from "react-hook-form";
 
 const ListadoInputsEntrega = [
   {
@@ -11,6 +13,14 @@ const ListadoInputsEntrega = [
     id: "nombrecompleto",
     type: "text",
     placeholder: "Ingresa tu nombre completo",
+    minLength: {
+      value: 4,
+      message: "La longitud minima es de 4 caracteres",
+    },
+    maxLength: {
+      value: 30,
+      message: "La longitud máxima es de 30 caracteres",
+    },
   },
   {
     nombreInput: "email",
@@ -18,27 +28,44 @@ const ListadoInputsEntrega = [
     id: "email",
     type: "email",
     placeholder: "Ingresa tu correo electrónico",
+    pattern: {
+      value: /^\S+@\S+$/i,
+      message: "Email no válido",
+    },
+    maxLength: {
+      value: 30,
+      message: "La longitud máxima es de 30 caracteres",
+    },
   },
   {
-    nombreInput: "confirmacion de email",
+    nombreInput: "teléfono",
     customStyles: "sm:w-[calc(50%-8px)]",
-    id: "emailconfirmation",
-    type: "email",
-    placeholder: "Confirma tu correo electrónico",
-  },
-  {
-    nombreInput: "número de teléfono",
-    customStyles: "",
     id: "telefono",
-    type: "tel",
+    type: "number",
     placeholder: "Ingresa tu número de telefono",
+    minLength: {
+      value: 4,
+      message: "La longitud minima es de 4 caracteres",
+    },
+    maxLength: {
+      value: 20,
+      message: "La longitud máxima es de 20 caracteres",
+    },
   },
   {
-    nombreInput: "Nombre de la calle y número de la casa",
+    nombreInput: "dirección",
     customStyles: "",
     id: "direccion",
     type: "text",
-    placeholder: "Nombre de la calle y número de la calle",
+    placeholder: "Nombre y número de la calle",
+    minLength: {
+      value: 4,
+      message: "La longitud minima es de 4 caracteres",
+    },
+    maxLength: {
+      value: 40,
+      message: "La longitud máxima es de 40 caracteres",
+    },
   },
   {
     nombreInput: "ciudad",
@@ -46,73 +73,116 @@ const ListadoInputsEntrega = [
     id: "ciudad",
     type: "text",
     placeholder: "Ciudad",
+    minLength: {
+      value: 4,
+      message: "La longitud minima es de 4 caracteres",
+    },
+    maxLength: {
+      value: 30,
+      message: "La longitud máxima es de 30 caracteres",
+    },
   },
   {
     nombreInput: "código postal",
     customStyles: "sm:w-[calc(50%-8px)]",
     id: "código postal",
-    type: "text",
+    type: "number",
     placeholder: "Ingresa tu código postal",
+    minLength: {
+      value: 4,
+      message: "La longitud minima es de 4 números",
+    },
+    maxLength: {
+      value: 10,
+      message: "La longitud máxima es de 10 caracteres",
+    },
   },
 ];
 
 interface Props {
-  direccionCorrecta: boolean;
   setDireccionCorrecta: React.Dispatch<React.SetStateAction<boolean>>;
 }
 
-export default function DireccionDeEntrega({
-  direccionCorrecta,
-  setDireccionCorrecta,
-}: Props) {
+export default function DireccionDeEntrega({ setDireccionCorrecta }: Props) {
+  const { register, handleSubmit, formState } = useForm();
+
   return (
-    <CustomFieldset
-      fieldsetCompleted={direccionCorrecta}
-      fieldName="direccion de entrega"
-      customStyles="md:col-start-1"
+    <form
+      onSubmit={handleSubmit((data) => {
+        localStorage.setItem("direccion", JSON.stringify(data));
+        setDireccionCorrecta(true);
+      })}
     >
-      <div className="flex flex-wrap gap-4 md:gap-y-6 xl:gap-y-8 px-2 mt-4">
-        {ListadoInputsEntrega.slice(0, 4).map((input) => (
-          <CustomInput
-            key={input.id}
-            nombreInput={input.nombreInput}
-            customStyles={input.customStyles}
-            id={input.id}
-            type={input.type}
-            placeholder={input.placeholder}
-          />
-        ))}
-        <Select
-          items={LISTADO_DE_PAISES}
-          label="país de envío"
-          placeholder="Selecciona un país"
-          radius="sm"
-          isRequired
-          labelPlacement="outside"
-          variant="bordered"
-          classNames={{
-            trigger: "border border-main-white/20",
-            label: "text-xs first-letter:uppercase lg:text-sm",
-            value: "text-xs ",
-          }}
-        >
-          {(pais) => (
-            <SelectItem key={pais.name} className="text-xs lg:text-sm">
-              {pais.es_name}
-            </SelectItem>
-          )}
-        </Select>
-        {ListadoInputsEntrega.slice(4, 7).map((input) => (
-          <CustomInput
-            key={input.id}
-            nombreInput={input.nombreInput}
-            customStyles={input.customStyles}
-            id={input.id}
-            type={input.type}
-            placeholder={input.placeholder}
-          />
-        ))}
-      </div>
-    </CustomFieldset>
+      <CustomFieldset
+        fieldsetError={formState.isSubmitted && !formState.isValid}
+        fieldsetCompleted={formState.isValid}
+        fieldName="direccion de entrega"
+        customStyles="md:col-start-1"
+      >
+        <div className="flex flex-wrap gap-4 md:gap-y-6 xl:gap-y-8 px-2 mt-4">
+          {ListadoInputsEntrega.slice(0, 3).map((input) => (
+            <CustomInput
+              key={input.id}
+              nombreInput={input.nombreInput}
+              customStyles={input.customStyles}
+              id={input.id}
+              type={input.type}
+              placeholder={input.placeholder}
+              errors={formState.errors}
+              register={register}
+              pattern={input.pattern}
+              minLength={input.minLength}
+              maxLength={input.maxLength}
+            />
+          ))}
+          <Select
+            {...register("pais", { required: "Este campo es obligatorio" })}
+            errorMessage={formState.errors.pais?.message as string | undefined}
+            isInvalid={formState.errors.pais?.message as boolean | undefined}
+            isRequired
+            items={LISTADO_PAISES_ENVIO}
+            label="país de envío"
+            placeholder="Selecciona un país"
+            radius="sm"
+            labelPlacement="outside"
+            variant="bordered"
+            classNames={{
+              trigger: "border border-main-white/20",
+              label: "text-xs first-letter:uppercase lg:text-sm",
+              value: "text-xs ",
+            }}
+          >
+            {(pais) => (
+              <SelectItem key={pais.name} className="text-xs lg:text-sm">
+                {pais.es_name}
+              </SelectItem>
+            )}
+          </Select>
+          {ListadoInputsEntrega.slice(3, 7).map((input) => (
+            <CustomInput
+              key={input.id}
+              nombreInput={input.nombreInput}
+              customStyles={input.customStyles}
+              id={input.id}
+              type={input.type}
+              placeholder={input.placeholder}
+              errors={formState.errors}
+              register={register}
+              pattern={input.pattern}
+              minLength={input.minLength}
+              maxLength={input.maxLength}
+            />
+          ))}
+        </div>
+      </CustomFieldset>
+
+      <button
+        type="submit"
+        className="w-full py-1 flex items-center justify-center gap-4 bg-primary text-main-black rounded-md font-bold uppercase mt-4"
+      >
+        <span>elegir envio</span>
+        <span className="icon-[tabler--arrow-narrow-right]"></span>
+      </button>
+    </form>
   );
 }
