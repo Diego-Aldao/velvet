38c0537b7f07e1948733a020f2e884b1afa7506d
Diego Aldao <diegofrontdev@gmail.com>
2025-01-10 19:41:49 -0300

    añadidas la constante con el listado de paises para el form direccion del checkout

--- constants.ts
+++ constants.ts
@@ -42,6 +42,101 @@ export const ENVIOS: Envio[] = [
   },
 ];
 
+export const LISTADO_PAISES_ENVIO = [
+  {
+    name: "Argentina",
+    es_name: "Argentina",
+  },
+  {
+    name: "Bolivia",
+    es_name: "Bolivia",
+  },
+  {
+    name: "Brazil",
+    es_name: "Brasil",
+  },
+  {
+    name: "Canada",
+    es_name: "Canadá",
+  },
+  {
+    name: "Chile",
+    es_name: "Chile",
+  },
+  {
+    name: "Colombia",
+    es_name: "Colombia",
+  },
+  {
+    name: "Costa Rica",
+    es_name: "Costa Rica",
+  },
+  {
+    name: "Cuba",
+    es_name: "Cuba",
+  },
+  {
+    name: "Dominican Republic",
+    es_name: "República Dominicana",
+  },
+  {
+    name: "Ecuador",
+    es_name: "Ecuador",
+  },
+  {
+    name: "El Salvador",
+    es_name: "El Salvador",
+  },
+  {
+    name: "Falkland Islands (Malvinas)",
+    es_name: "Islas Malvinas",
+  },
+  {
+    name: "Guatemala",
+    es_name: "Guatemala",
+  },
+  {
+    name: "Honduras",
+    es_name: "Honduras",
+  },
+  {
+    name: "Mexico",
+    es_name: "México",
+  },
+  {
+    name: "Panama",
+    es_name: "Panamá",
+  },
+  {
+    name: "Paraguay",
+    es_name: "Paraguay",
+  },
+  {
+    name: "Peru",
+    es_name: "Perú",
+  },
+  {
+    name: "Puerto Rico",
+    es_name: "Puerto Rico",
+  },
+  {
+    name: "Spain",
+    es_name: "España",
+  },
+  {
+    name: "United States",
+    es_name: "Estados Unidos",
+  },
+  {
+    name: "Uruguay",
+    es_name: "Uruguay",
+  },
+  {
+    name: "Venezuela",
+    es_name: "Venezuela",
+  },
+];
+
 export const BASE_URL_PRODUCTOS = `https://asos2.p.rapidapi.com/products/v2/list?store=ES&lang=es-ES&country=ES&currency=EUR&sizeSchema=ES&`;
 export const BASE_URL_CATEGORIAS = `https://asos2.p.rapidapi.com/categories/list?country=ES&lang=es-ES`;
 export const BASE_URL_DETALLE_PRODUCTO = `https://asos2.p.rapidapi.com/products/v4/detail?`;
