73a20fb670dd71b119f1ebb06642125797895089
Diego Aldao <diegofrontdev@gmail.com>
2025-01-17 17:40:57 -0300

    corregidos los estilos y la informacion del banner

--- components/PageInicio/Oferta.tsx
+++ components/PageInicio/Oferta.tsx
@@ -1,32 +1,20 @@
-"use client";
-import React, { useState } from "react";
-import { MainButtonInteractive } from "../buttons/MainButton";
-import ModalRegistro from "../registro/ModalRegistro";
+import React from "react";
 
 export default function Oferta() {
-  const [popUpVisible, setPopUpVisible] = useState<boolean>(false);
-
-  const handleClick = () => {
-    setPopUpVisible(true);
-  };
-
   return (
     <section className="flex px-4 md:px-10 lg:px-12 flex-col gap-4 pt-12 pb-4 items-center justify-center mb-20 bg-secondary text-center">
-      <span className="uppercase text-sm font-nunito-sans text-main-white font-bold lg:text-base ">
+      <span className="capitalize italic text-lg font-playfair-display text-main-white md:text-xl lg:text-2xl">
         velvet premier party
       </span>
-      <span className="uppercase text-main-white text-3xl max-w-[300px] md:max-w-full sm:text-3xl font-bold md:text-4xl">
-        hazte miembro por el 25% en todo mañana!
+      <span className="uppercase text-main-white text-4xl max-w-[300px] md:max-w-full sm:text-3xl font-black md:text-5xl">
+        -25% en toda la tienda!
       </span>
-      <MainButtonInteractive
-        nombreDestino="registrarme"
-        handleClick={handleClick}
-        customStyles="px-6 py-2 uppercase !text-sm lg:text-base"
-      />
-      <ModalRegistro
-        modalVisible={popUpVisible}
-        setModalVisible={setPopUpVisible}
-      />
+      <span className="first-letter:uppercase">
+        con el código:{" "}
+        <span className="inline-block py-1 px-3 border-2 rounded-sm border-secondary-black uppercase font-bold text-lg">
+          party
+        </span>
+      </span>
       <span className="text-[10px] text-main-white font-medium mt-4">
         Se aplican terminos y condiciones. Solo por tiempo limitado
       </span>
